fa002953c782e870abd41bb0327e3156798475b0
Krur Schak <krurschak@gmail.com>
2021-04-18 10:30:04 +0200

    Initial code commit

---
+++ src/DR.Services.Users/Controllers/DefaultController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DR.Services.Users.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+            => Ok("DR Users Service");
+    }
+}
