34ae4906bc7bb4aab428aeff68ba3334e6f3d806
Krur Schak <krurschak@gmail.com>
2021-05-08 13:20:04 +0200

    add masstransit and user controller using components user dto

---
+++ src/DR.Services.Users/Controllers/UsersController.cs
@@ -0,0 +1,118 @@
+﻿using DR.Frameworks.Users.Models;
+using DR.Packages.Mongo.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DR.Services.Users.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly ILogger<UsersController> logger;
+        private readonly IMongoRepository<User> userRepository;
+
+        public UsersController(
+            ILogger<UsersController> logger,
+            IMongoRepository<User> userRepository)
+        {
+            this.logger = logger;
+            this.userRepository = userRepository;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Components.Users.Dto.User> Get([FromRoute] Guid id)
+        {
+            try
+            {
+                var user = await userRepository.GetAsync(id);
+
+                if (user is null)
+                {
+                    logger.LogWarning($"user not found with id: {id}");
+                    return null;
+                }
+
+                logger.LogInformation($"project found with id: {id}");
+                return new Components.Users.Dto.User
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Salutation = user.Salutation,
+                    Title = user.Title,
+                    LastName = user.LastName,
+                    FirstName = user.FirstName
+                };
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.ToString());
+                return null;
+            }
+        }
+
+        [HttpGet("[action]")]
+        public async Task<Components.Users.Dto.User> GetByIdentity([FromQuery] string sub, [FromQuery] string iss)
+        {
+            try
+            {
+                var user = await userRepository.GetAsync(x => x.IdentitySub == sub && x.IdentityIss == iss);
+
+                if (user is null)
+                {
+                    return null;
+                }
+
+                return new Components.Users.Dto.User
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Salutation = user.Salutation,
+                    Title = user.Title,
+                    LastName = user.LastName,
+                    FirstName = user.FirstName
+                };
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.ToString());
+                return null;
+            }
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IEnumerable<Components.Users.Dto.User>> Get([FromQuery] IEnumerable<Guid> ids = null)
+        {
+            try
+            {
+                if (ids is null || !ids.Any())
+                {
+                    logger.LogWarning("list of user ids cannot be null or empty");
+                    return null;
+                }
+
+                var users = await userRepository.FindAsync(x => ids.Contains(x.Id));
+
+                logger.LogInformation($"{users.Count()} projects found");
+                return users.Select(x => new Components.Users.Dto.User
+                {
+                    Id = x.Id,
+                    Email = x.Email,
+                    Salutation = x.Salutation,
+                    Title = x.Title,
+                    LastName = x.LastName,
+                    FirstName = x.FirstName
+                });
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.ToString());
+                return null;
+            }
+        }
+    }
+}
--- src/DR.Services.Users/Startup.cs
+++ src/DR.Services.Users/Startup.cs
@@ -5,12 +5,14 @@ using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -45,22 +47,33 @@ namespace DR.Services.Users
                 cfg.AddMongoRepository<User>("Users");
             });
 
-            services
-                .AddMassTransit(cfg =>
-                {
-                    cfg.AddConsumers(Assembly.GetExecutingAssembly());
-                    cfg.UseRabbitMq(Configuration.GetConnectionString("RabbitMq"), Assembly.GetExecutingAssembly());
-                })
-                .AddMassTransitHostedService();
+            // Register MassTransit and Subscriptions if RabbitMq Connection existing (read only if not)
+            var rabbitMq = Configuration.GetConnectionString("RabbitMq");
+            if (string.IsNullOrEmpty(rabbitMq))
+            {
+                services
+                    .AddMassTransit(x =>
+                    {
+                        x.AddConsumers(Assembly.GetExecutingAssembly());
+                        x.UseRabbitMq(rabbitMq, Assembly.GetExecutingAssembly());
+                    })
+                    .AddMassTransitHostedService();
+            }
 
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DR User Service" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (!env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DR User Service"));
             }
 
             app.UseRouting();
@@ -75,6 +88,14 @@ namespace DR.Services.Users
                 });
 
                 endpoints.MapHealthChecks("/health/live", new HealthCheckOptions());
+
+                if (env.IsProduction())
+                {
+                    endpoints.MapGet("/", async context =>
+                    {
+                        await context.Response.WriteAsync("DR User Service");
+                    });
+                }
 
                 endpoints.MapControllers();
             });
